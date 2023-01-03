3b9b762da7f834f39ad7edb1280a364448d8d340
Kiril Grigoriev <95177550+kirogr@users.noreply.github.com>
2023-01-03 23:38:46 +0200

    🐼
    not necessary

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <stdint.h>
 #include<sys/time.h>
 
-long long tseed(void) {
+long tseed(void) {
     struct timeval tv;
     gettimeofday(&tv,NULL);
     return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
